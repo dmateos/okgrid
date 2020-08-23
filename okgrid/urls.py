3156e5247d46df2f8f49c764a2488a87f7079577
Daniel Mateos <daniel@mateos.cc>
2020-08-24 02:05:52 +1000

    start on templates

--- okgrid/urls.py
+++ okgrid/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+
 import grid.api as api
+import grid.views as views
 
 router = routers.DefaultRouter()
 router.register(r"grids", api.GridViewSet)
@@ -10,4 +12,5 @@ router.register(r"gridelements", api.GridElementViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('', views.root),
 ]
