7233c4c60524f4fb62f0969b239114823466abbd
Daniel Mateos <daniel@mateos.cc>
2020-08-24 01:10:31 +1000

    start on tests

--- grid/tests.py
+++ grid/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+import pytest
+from .models import Grid
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_grid_create():
+    User.objects.create_user(username="test", password="p455w0rd123")
+    client = APIClient()
+    client.login(username="test", password="p455w0rd123")
+
+    response = client.post("/api/grids/", {"name": "testgrid"}, format="json")
+    assert response.status_code == 201
+
+
+def test_grid_create_unauthenticated():
+    client = APIClient()
+    response = client.post("/api/grids/", {"name": "testgrid"}, format="json")
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_grid_get():
+    User.objects.create_user(username="test", password="p455w0rd123")
+
+    client = APIClient()
+    client.login(username="test", password="p455w0rd123")
+
+    response = client.get("/api/grids/1/")
+    assert response.status_code == 404
+
+    Grid.objects.create(name="TestGrid")
+    response = client.get("/api/grids/1/")
+    assert response.status_code == 200
